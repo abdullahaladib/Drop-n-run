0539fcd12946c896226252f0f2c83c2107801070
MD Rashed Karim <rashed@MDs-MacBook-Pro.local>
2025-12-30 06:26:59 +0600

    switch device
    asdf

--- dropnrun.py
+++ dropnrun.py
@@ -3,3 +3,27 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import math
 import random
+
+camera_pos = (0,500,500)
+
+class player:
+  pass
+
+class mob:
+  pass
+
+def setup_camera():
+  glMatrixMode(GL_PROJECTION)
+  glLoadIdentity()
+  gluPerspective(120, 1.25, 0.1, 1500)
+  glMatrixMode(GL_MODELVIEW)
+
+  x,y,z = camera_pos
+
+  gluLookAt(x,y,z,
+            0,0,0,
+            0,0,1)
+
+def idle():
+  glutPostRedisplay()
+
---
+++ shapes.py
@@ -0,0 +1,56 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import math
+import random
+
+
+def pitch():
+  glBegin(GL_QUADS)
+
+  glColor3f(1,1,1)
+  glVertex3f(600, 600, 0)
+  glVertex3f(-600, 600, 0)
+  glVertex3f(-600, -600, 0)
+  glVertex3f(600, -600, 0)  
+
+def wall():
+    glBegin(GL_QUADS)
+    glColor3f(0,0.65,1)
+    glVertex3f(-10000,-600, 10000)
+    glVertex3f(-10000,-600, 0)
+    glVertex3f(10000,-600, 10000)
+    glVertex3f(10000,-600, 0)
+
+    glEnd()
+
+def background():
+    glBegin(GL_QUADS)
+    glColor3f(0,0.45,0)
+    glVertex3f(-10000,600, 0)
+    glVertex3f(-600,600, 0)
+    glVertex3f(-600,-600, 0)
+    glVertex3f(-10000,-600, 0)
+
+    glEnd()
+
+    glBegin(GL_QUADS)
+    glColor3f(0,0.45,0)
+    glVertex3f(10000,600, 0)
+    glVertex3f(600,600, 0)
+    glVertex3f(600,-600, 0)
+    glVertex3f(10000,-600, 0)
+
+    glEnd()
+    glLineWidth(40)
+    glBegin(GL_LINES)
+    glColor3f(0,0,0)
+    glVertex3f(-600,-600, 0)
+    glVertex3f(-600,600,0)
+    glEnd()
+
+    glBegin(GL_LINES)
+    glColor3f(0,0,0)
+    glVertex3f(600,-600, 0)
+    glVertex3f(600,600,0)
+    glEnd()
