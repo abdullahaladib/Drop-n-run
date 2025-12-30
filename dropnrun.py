0bf2e2d33fe286bd21441fb84516da5a6fd6196c
Abdullah Al Adib <mail4adib@gmail.com>
2025-12-30 16:10:26 +0600

    view created

--- dropnrun.py
+++ dropnrun.py
@@ -3,27 +3,91 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import math
 import random
+import shapes
+
 
 camera_pos = (0,500,500)
+player_x = 0
+player_z = 0
+player_y = 60  # place player nearer the camera line for a TPP view
 
-class player:
-  pass
+def draw_player():
+  glPushMatrix()
+  glTranslatef(player_x, player_y, player_z)
+  glRotatef(90, 1, 0, 0)  # orient model so z is up
+  glScalef(150, 150, 150)
 
-class mob:
-  pass
+  glColor3f(0, 0, 1)
+  for o in [-0.2, 0.2]:
+    glPushMatrix()
+    glTranslatef(o, 0.4, 0)
+    glRotatef(-90, 1, 0, 0)
+    gluCylinder(gluNewQuadric(), 0.1, 0.05, 0.4, 10, 10)
+    glPopMatrix()
+
+  glColor3f(0.4, 0.5, 0.2)
+  glPushMatrix()
+  glTranslatef(0, 1, 0)
+  glScalef(0.4, 0.6, 0.2)
+  glutSolidCube(1)
+  glPopMatrix()
+
+  glColor3f(0.96, 0.8, 0.69)
+  for o in [-0.15, 0.15]:
+    glPushMatrix()
+    glTranslatef(o, 1.2, 0.1)
+    gluCylinder(gluNewQuadric(), 0.1, 0.05, 0.4, 10, 10)
+    glPopMatrix()
+
+  glColor3f(0.5, 0.5, 0.5)
+  glPushMatrix()
+  glTranslatef(0, 1.2, 0.1)
+  gluCylinder(gluNewQuadric(), 0.08, 0, 0.8, 10, 10)
+  glPopMatrix()
+
+  glColor3f(0, 0, 0)
+  glPushMatrix()
+  glTranslatef(0, 1.65, 0)
+  gluSphere(gluNewQuadric(), 0.2, 10, 10)
+  glPopMatrix()
+
+  glPopMatrix()
 
 def setup_camera():
   glMatrixMode(GL_PROJECTION)
   glLoadIdentity()
   gluPerspective(120, 1.25, 0.1, 1500)
   glMatrixMode(GL_MODELVIEW)
-
-  x,y,z = camera_pos
-
-  gluLookAt(x,y,z,
-            0,0,0,
+  cam_x, cam_y, cam_z = camera_pos
+  gluLookAt(cam_x, cam_y, cam_z,
+            player_x, player_y, player_z,
             0,0,1)
 
 def idle():
   glutPostRedisplay()
 
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    glViewport(0, 0, 1000, 800)
+    
+    setup_camera()
+    
+    shapes.pitch()
+    shapes.wall()
+    shapes.background()
+    
+    draw_player()
+    
+    
+    glutSwapBuffers()
+    
+    
+glutInit()
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+glutInitWindowSize(1000, 800)
+wind = glutCreateWindow(b"Drop 'n' Run")
+glutDisplayFunc(showScreen)
+glutIdleFunc(idle)
+glutInitWindowPosition(0,0)
+glutMainLoop()
--- shapes.py
+++ shapes.py
@@ -13,15 +13,24 @@ def pitch():
   glVertex3f(-600, 600, 0)
   glVertex3f(-600, -600, 0)
   glVertex3f(600, -600, 0)  
+  
+  glEnd()
 
 def wall():
     glBegin(GL_QUADS)
     glColor3f(0,0.65,1)
-    glVertex3f(-10000,-600, 10000)
     glVertex3f(-10000,-600, 0)
-    glVertex3f(10000,-600, 10000)
     glVertex3f(10000,-600, 0)
+    glVertex3f(10000,-600, 10000)
+    glVertex3f(-10000,-600, 10000)
 
+    glEnd()
+    
+    glLineWidth(40)
+    glBegin(GL_LINES)
+    glColor3f(0,0,0)
+    glVertex3f(-10000,-600, 0)
+    glVertex3f(10000,-600,0)
     glEnd()
 
 def background():
